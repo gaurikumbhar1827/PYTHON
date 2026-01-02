5d15b2159f6a78f42d2765ffa8052e2b70a8c5dd
Gauri Kumbhar <gaurikumbhar2007@gmail.com>
2026-01-02 20:12:00 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,10 @@
+def Hi():
+    print("Hi")
+Hi()
+def wel():
+    print("Welcome to Crochet World ")
+wel()
+a=input("Please Enter your Name: ")
+def great():
+    print("Great to see you "+a)
+great()
